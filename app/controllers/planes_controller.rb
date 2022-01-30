b4ea8578926ac09f4bec52742e0e2fab6fb9308b
Kevin Urbaum <kevin.urbaum@gmail.com>
2022-01-30 19:32:32 +0100

    added select2, created simple create view and controller for plane creation

--- app/controllers/planes_controller.rb
+++ app/controllers/planes_controller.rb
@@ -1,5 +1,28 @@
 class PlanesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
   def index
     @planes = Plane.all
+  end
+
+  def new
+    @plane = Plane.new
+  end
+
+  def create
+    @plane = Plane.new(plane_params)
+    @user = current_user
+    @plane.user = @user
+    if @plane.save
+      redirect_to plane_path(@plane)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def plane_params
+    params.require(:plane).permit(:name, :price, :availability, :plane_model_id)
   end
 end
--- app/models/plane.rb
+++ app/models/plane.rb
@@ -3,4 +3,6 @@ class Plane < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_one_attached :photo
+
+  validates :name, :availability, presence: true
 end
