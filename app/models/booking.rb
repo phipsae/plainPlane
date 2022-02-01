f44d88a1dfe3fdbf4929cc7bf56ec824a86f7536
Philip Krause <philip.krause88@yahoo.de>
2022-02-01 21:39:19 +0100

    heroooookuuuuu

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,7 +3,7 @@ class Booking < ApplicationRecord
   belongs_to :user
 
   enum status: { pending: 0, confirmed: 1, declined: 2, accepted: 3, cancelled: 4, paid: 5 }
-  validates :end_date, comparison: { greater_than_or_equal_to: :start_date }
+  # validates :end_date, comparison: { greater_than_or_equal_to: :start_date }
 
   validates :status, :start_date, :end_date, presence: true
 end
