d79ca27a4dcf3c5b3e107aa6d3a61cf75fbacaa3
Philip Krause <philip.krause88@yahoo.de>
2022-02-01 22:14:16 +0100

    seeeed with pictures

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :bookings
-  has_many :planes
+  has_many :bookings, dependent: :destroy
+  has_many :planes, dependent: :destroy
 end
