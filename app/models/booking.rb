ff08a55799720183f7ba40db3f544793a46daabb
Kevin Urbaum <kevin.urbaum@gmail.com>
2022-02-02 21:59:22 +0100

    added simple start end date validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,4 +6,16 @@ class Booking < ApplicationRecord
   # validates :end_date, comparison: { greater_than_or_equal_to: :start_date }
 
   validates :status, :start_date, :end_date, presence: true
+
+  validate :end_date_is_after_start_date
+
+  private
+
+  def end_date_is_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "cannot be before the start date")
+    end
+  end
 end
