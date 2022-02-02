c425d737f6cc6708be53da847540c5ad7067c604
Kevin Urbaum <kevin.urbaum@gmail.com>
2022-02-02 22:00:42 +0100

    refactored booking start end date validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -14,8 +14,6 @@ class Booking < ApplicationRecord
   def end_date_is_after_start_date
     return if end_date.blank? || start_date.blank?
 
-    if end_date < start_date
-      errors.add(:end_date, "cannot be before the start date")
-    end
+    errors.add(:end_date, "cannot be before the start date") if end_date < start_date
   end
 end
