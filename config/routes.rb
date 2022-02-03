543b6ccca677fdea1ae891d519e5c7a999880180
Philip Krause <philip.krause88@yahoo.de>
2022-02-03 15:04:19 +0100

    seed adjusted

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :planes, except: [ :destroy ] do
+  resources :planes do
     resources :bookings, only: [ :create ]
-
   end
   resources :bookings, only: [ :index, :edit, :update, :show ]
   resources :plane_models, only: [ :create, :update, :edit ]
