190c891c295b058aa5b46f4d3dc113d8e0973a23
Philip Krause <philip.krause88@yahoo.de>
2022-02-05 12:02:14 +0100

    pundit working

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -1,7 +1,11 @@
 class BookingPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.where(user: user)   # @record.plane.user
+      scope.all  # where(user: user) # @record.plane.user
     end
+  end
+
+  def create?
+    user
   end
 end
