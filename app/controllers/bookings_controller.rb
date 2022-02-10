6720c04d53531884ba46412e8fc78161de376788
Kevin Urbaum <kevin.urbaum@gmail.com>
2022-02-10 21:54:04 +0100

    first changes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,6 +4,7 @@ class BookingsController < ApplicationController
   def index
     @bookings = policy_scope(Booking) # current_user.bookings policy_scope(Booking)
     @user_planes = current_user.planes
+    @sorted_bookings = @bookings.order('start_date ASC')
   end
 
   def show; end
